8f73cc82cb565d1688dd7f84ffd0a900a14e04f7
roshangurung <r.gurung1992@gmail.com>
2019-12-10 21:22:04 -0800

    Initial commit

---
+++ Program to Demonstrate DivideByZero Exception/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()///This C# program is used to demonstrate DivideByZero exception. In try statement using ‘result’ variable divide the value of 15 by 0, here a DivideByZero exception is thrown. It indicates that a statement attempted to evaluate a division by zero. Using try and catch, an error message is displayed when the error occurs.////
+    {
+        try
+        {
+            int result = 15 / int.Parse("0");
+            Console.WriteLine(result);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.Write(e.Message);
+            Console.ReadLine();
+        }
+    }
+}
+///Output: Attempted to Divide by Zero
